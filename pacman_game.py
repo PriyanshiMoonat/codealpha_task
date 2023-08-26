4498e175f2a9f26c7e5eb19ce0ad4d9079666bb9
PriyanshiMoonat <128178318+PriyanshiMoonat@users.noreply.github.com>
2023-08-26 22:03:48 +0530

    Add files via upload

---
+++ pacman_game.py
@@ -0,0 +1,145 @@
+from random import choice
+from turtle import *
+from freegames import floor, vector
+scores = {'score': 0}
+road = Turtle(visible=False)
+writer = Turtle(visible=False)
+target = vector(5, 0)
+pacman = vector(-40, -80)
+ghosts = [
+    [vector(-180, 160), vector(5, 0)],
+    [vector(-180, -160), vector(0, 5)],
+    [vector(100, 160), vector(0, -5)],
+    [vector(100, -160), vector(-5, 0)],
+]
+# fmt: off
+grounds = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+# fmt: on
+def square(x, y):
+    """Draw square using road at (x, y)."""
+    road.up()
+    road.goto(x, y)
+    road.down()
+    road.begin_fill()
+    for count in range(4):
+        road.forward(20)
+        road.left(90)
+    road.end_fill()
+def offset(point):
+    """Return offset of point in grounds."""
+    x = (floor(point.x, 20) + 200) / 20
+    y = (180 - floor(point.y, 20)) / 20
+    index = int(x + y * 20)
+    return index
+def valid(point):
+    """Return True if point is valid in grounds."""
+    index = offset(point)
+
+    if grounds[index] == 0:
+        return False
+
+    index = offset(point + 19)
+
+    if grounds[index] == 0:
+        return False
+
+    return point.x % 20 == 0 or point.y % 20 == 0
+def world():
+    """Draw world using road."""
+    bgcolor('black')
+    road.color('#839192')
+
+    for index in range(len(grounds)):
+        ground = grounds[index]
+
+        if ground > 0:
+            x = (index % 20) * 20 - 200
+            y = 180 - (index // 20) * 20
+            square(x, y)
+
+            if ground == 1:
+                road.up()
+                road.goto(x + 10, y + 10)
+                road.dot(2, 'white')
+def move():
+    """Move pacman and all ghosts."""
+    writer.undo()
+    writer.write(scores['score'])
+
+    clear()
+
+    if valid(pacman + target):
+        pacman.move(target)
+
+    index = offset(pacman)
+
+    if grounds[index] == 1:
+        grounds[index] = 2
+        scores['score'] += 1
+        x = (index % 20) * 20 - 200
+        y = 180 - (index // 20) * 20
+        square(x, y)
+    up()
+    goto(pacman.x + 10, pacman.y + 10)
+    dot(20, 'yellow')
+    for point, course in ghosts:
+        if valid(point + course):
+            point.move(course)
+        else:
+            options = [
+                vector(5, 0),
+                vector(-5, 0),
+                vector(0, 5),
+                vector(0, -5),
+            ]
+            plan = choice(options)
+            course.x = plan.x
+            course.y = plan.y
+        up()
+        goto(point.x + 10, point.y + 10)
+        dot(20, 'red')
+    update()
+    for point, course in ghosts:
+        if abs(pacman - point) < 20:
+            return
+    ontimer(move, 100)
+def change(x, y):
+    """Change pacman target if valid."""
+    if valid(pacman + vector(x, y)):
+        target.x = x
+        target.y = y
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+writer.goto(160, 160)
+writer.color('white')
+writer.write(scores['score'])
+listen()
+onkey(lambda: change(5, 0), 'Right')
+onkey(lambda: change(-5, 0), 'Left')
+onkey(lambda: change(0, 5), 'Up')
+onkey(lambda: change(0, -5), 'Down')
+world()
+move()
+done()
